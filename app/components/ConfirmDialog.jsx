c5b4b5fe605db6fdb440c27995d914e57153908f
naknamu <jodeldelvalle@gmail.com>
2024-03-08 14:28:40 +0800

    Added delete image functionality

---
+++ app/components/ConfirmDialog.jsx
@@ -0,0 +1,44 @@
+import style from "./ConfirmDialog.module.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { getCookie } from "cookies-next";
+
+export default function ConfirmDialog({ setIsDialog, image }) {
+  const router = useRouter();
+  const { data } = useSession();
+  const auth_token = getCookie("auth_token");
+
+  const handleDeleteImage = async () => {
+    // Call Delete image API
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/image/${image._id}/delete`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${auth_token}`,
+        },
+      }
+    );
+
+    if (res.status === 200) {
+      router.push(`/uploader/${data.user.name}`);
+      router.refresh();
+    }
+  };
+
+  return (
+    <div className={style.overlay}>
+      <div className={style.container}>
+        <b>Are you sure you want to delete this image?</b>
+        <span className={style.btngroup}>
+          <button className="btn_primary" onClick={handleDeleteImage}>
+            Confirm
+          </button>
+          <button className="btn_primary" onClick={() => setIsDialog(false)}>
+            Cancel
+          </button>
+        </span>
+      </div>
+    </div>
+  );
+}
--- app/components/Panel.jsx
+++ app/components/Panel.jsx
@@ -1,15 +1,23 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import style from "./Panel.module.css";
 import { useRouter } from "next/navigation";
+import ConfirmDialog from "./ConfirmDialog";
 
 export default function Panel({ image }) {
   const router = useRouter();
+  const [isDialog, setIsDialog] = useState(false);
 
   const handleEditImage = () => {
     // CALL UPDATE IMAGE FORM
     router.push(`/images/${image.slug}/update`)
+  }
+
+  const handleDeleteImage = () => {
+    // Enable a dialog confirmation
+    setIsDialog(true);
+    // Delete image logic
   }
 
   return (
@@ -18,10 +26,11 @@ export default function Panel({ image }) {
         <span className="material-symbols-outlined">edit</span>
         <span>Edit</span>
       </div>
-      <div className={style.delete}>
+      <div className={style.delete} onClick={handleDeleteImage}>
         <span className="material-symbols-outlined">delete</span>
         <span>Delete</span>
       </div>
+      {isDialog && <ConfirmDialog setIsDialog={setIsDialog} image={image} />}
     </div>
   );
 }
