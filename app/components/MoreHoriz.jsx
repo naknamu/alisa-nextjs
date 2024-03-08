20baf4190c5a3731f6ea00a4e26558bf5885f63e
naknamu <jodeldelvalle@gmail.com>
2024-03-08 10:21:43 +0800

    Added hook to detect click outside panel component

--- app/components/MoreHoriz.jsx
+++ app/components/MoreHoriz.jsx
@@ -4,10 +4,14 @@ import { useState } from "react";
 import style from "./MoreHoriz.module.css";
 import Panel from "./Panel";
 import { useSession } from "next-auth/react";
+import { useClickAway } from "@uidotdev/usehooks";
 
 export default function MoreHoriz({ image }) {
   const [togglePanel, setTogglePanel] = useState(false);
   const { data } = useSession();
+  const ref = useClickAway(() => {
+    setTogglePanel(false);
+  });
 
   const handleMorehoriz = () => {
     // Toggle Panel
@@ -19,7 +23,7 @@ export default function MoreHoriz({ image }) {
   }
 
   return (
-    <div className={style.container}>
+    <div className={style.container} ref={ref}>
         <span className="material-symbols-outlined" onClick={handleMorehoriz}>more_horiz</span>
         {/**Enable edit and delete functionality on uploader's own images **/}
         {togglePanel && (data?.user?.name === image.uploader.slug) && <Panel image={image} />}
