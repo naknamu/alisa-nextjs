50869d340946ac44b61b8228bb7f8cc1d78a8424
naknamu <jodeldelvalle@gmail.com>
2024-03-08 13:40:16 +0800

    Removed hook and reverted to server component

--- app/components/ImageCards.jsx
+++ app/components/ImageCards.jsx
@@ -1,29 +1,18 @@
-"use client"
-
 import Link from "next/link";
 import style from "./ImageCards.module.css";
 import DateUploaded from "./DateUploaded";
 import MoreHoriz from "./MoreHoriz";
 
-import { useSession } from "next-auth/react";
-import { useIntersectionObserver } from "@uidotdev/usehooks";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default function ImageCards({ images }) {
-
-  const { data } = useSession();
-
-  const [ref, entry] = useIntersectionObserver({
-    threshold: 0,
-    root: null,
-    rootMargin: "0px",
-  });
+export default async function ImageCards({ images }) {
+  const session = await getServerSession(authOptions);
 
   return (
-    <div className={style.container} ref={ref}>
+    <div className={style.container}>
       {images.map((image) => (
         <div key={image._id} className={style.card}>
-                  {entry?.isIntersecting && (
-                    <>
           <div className={style.header}>
             <div className={style.left_header}>
               <Link href={`/uploader/${image.uploader.slug}`} className="link">
@@ -33,16 +22,15 @@ export default function ImageCards({ images }) {
               <div className={style.date}>
                 <DateUploaded date={image.createdAt} />
               </div>
-            </div >
+            </div>
             <div className={style.right_header}>
-              {data.user.name && <MoreHoriz image={image}/>}
+              {session && <MoreHoriz image={image} />}
             </div>
           </div>
           <h3>{image.caption}</h3>
           <Link href={`/images/${image.slug}`}>
             <img className={style.img} src={image.source} alt={image.caption} />
           </Link>
-          </>)}
         </div>
       ))}
     </div>
