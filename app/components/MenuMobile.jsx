c36a0bb2a3b8adf5d329aa0837846af3b4be5b3d
naknamu <jodeldelvalle@gmail.com>
2024-03-16 12:15:54 +0800

    Changed navbar to server component

---
+++ app/components/MenuMobile.jsx
@@ -0,0 +1,64 @@
+"use client";
+
+import ProfileBtn from "./ProfileBtn";
+import Footer from "./Footer";
+import CategoryBtn from "./CategoryBtn";
+import { IoIosMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+import style from "./MenuMobile.module.css";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import LogBtn from "./LogBtn";
+
+export default function MenuMobile({ categories }) {
+  const { data } = useSession();
+  const [isMenu, setIsMenu] = useState(false);
+
+  const handleMenu = async () => {
+    if (isMenu) {
+      setIsMenu(false);
+    } else {
+      setIsMenu(true);
+    }
+  };
+
+  return (
+    <div>
+      <div className={`icon ${style.menubtn}`} onClick={() => handleMenu()}>
+        <IoIosMenu />
+      </div>
+      {isMenu && 
+        <div className={style.menu_overlay}>
+          <div className={style.menu_content}>
+
+            <div className={style.menu_content_head}>
+              <div onClick={() => setIsMenu(false)}>
+                {data && <ProfileBtn uploaderName={data.user.name} />}
+              </div>
+              <div className="icon" onClick={() => setIsMenu(false)}>
+                <IoMdClose />
+              </div>
+            </div>
+
+            <div
+              className={style.menu_btn_group}
+              onClick={() => setIsMenu(false)}
+            >
+              {data && (
+                <Link
+                  href={`/uploader/${data.user.name}/upload`}
+                  className={style.uploadbtn}
+                >
+                  <b>Upload Image</b>
+                </Link>
+              )}
+              <CategoryBtn categories={categories} />
+              <LogBtn />
+              <Footer />
+            </div>
+          </div>
+        </div>
+      }
+    </div>
+  );
+}
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,78 +1,23 @@
-"use client";
-
 import Link from "next/link";
 import style from "./Navbar.module.css";
-import { useSession } from "next-auth/react";
-import CategoryBtn from "./CategoryBtn";
-import { useState } from "react";
 import LogBtn from "./LogBtn";
-import ProfileBtn from "./ProfileBtn";
-import Footer from "./Footer";
-import { getCategories } from "@/app/actions.js"
-import { IoIosMenu } from "react-icons/io";
-import { IoMdClose } from "react-icons/io";
+import { getCategories } from "@/app/actions.js";
+import MenuMobile from "./MenuMobile";
 
-export default function Navbar() {
-  const { data } = useSession();
-  const [categories, setCategories] = useState(null);
-  const [isMenu, setIsMenu] = useState(false);
-
-  const handleMenu = async () => {
-
-    const data = await getCategories();
-    setCategories(data);
-
-    if (isMenu) {
-      setIsMenu(false);
-    } else {
-      setIsMenu(true);
-    }
-  };
+export default async function Navbar() {
+  const categories = await getCategories();
 
   return (
     <nav className={style.nav}>
       <Link href="/">
         <h1 className={style.logo}>Alisa</h1>
       </Link>
-      <div className={style.log_btn_dektop}>
+      <div className={style.btn_desktop}>
         <LogBtn />
       </div>
-
-      <div className={`icon ${style.menubtn}`} onClick={() => handleMenu()}>
-        <IoIosMenu />
+      <div className={style.btn_mobile}>
+        <MenuMobile categories={categories} />
       </div>
-      {isMenu && (
-        <div className={style.menu_overlay}>
-          <div className={style.menu_content}>
-            <div className={style.menu_content_head}>
-              <div onClick={() => setIsMenu(false)}>
-                {data && <ProfileBtn uploaderName={data.user.name} />}
-              </div>
-              <div className="icon" onClick={() => setIsMenu(false)}>
-                <IoMdClose />
-              </div>
-            </div>
-            <div
-              className={style.menu_btn_group}
-              onClick={() => setIsMenu(false)}
-            >
-              {data && (
-                <Link
-                  href={`/uploader/${data.user.name}/upload`}
-                  className={style.uploadbtn}
-                >
-                  <b>Upload Image</b>
-                </Link>
-              )}
-              {categories && <CategoryBtn categories={categories} />}
-              <div>
-                <LogBtn />
-              </div>
-              <Footer />
-            </div>
-          </div>
-        </div>
-      )}
     </nav>
   );
 }
