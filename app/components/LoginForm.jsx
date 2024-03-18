10138230c26e9b6618d69e111b1c25b396d4f28c
naknamu <jodeldelvalle@gmail.com>
2024-03-18 10:57:56 +0800

    Converted login page to static content

---
+++ app/components/LoginForm.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import style from "./LoginForm.module.css";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LoginForm() {
+  const [uploaderInfo, setUploaderInfo] = useState({ email: "", password: "" });
+  const [isError, setIsError] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await signIn("credentials", {
+      email: uploaderInfo.email,
+      password: uploaderInfo.password,
+      redirect: false,
+    });
+
+    if (res.status === 200) {
+      console.log(res);
+      router.push("/");
+      router.refresh();
+      setIsError(false);
+    } else {
+      console.log(res.error);
+      setIsError(true);
+    }
+  };
+
+  return (
+    <div className={style.form_wrapper}>
+      <form onSubmit={handleSubmit} className={style.form}>
+        <div>
+          <label htmlFor="emailOrUsername">Email or Username</label>
+          <input
+            className={style.input}
+            type="text"
+            name="emailOrUsername"
+            id="emailOrUsername"
+            value={uploaderInfo.email}
+            onChange={({ target }) =>
+              setUploaderInfo({ ...uploaderInfo, email: target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            className={style.input}
+            type="password"
+            name="password"
+            id="password"
+            value={uploaderInfo.password}
+            onChange={({ target }) =>
+              setUploaderInfo({ ...uploaderInfo, password: target.value })
+            }
+          />
+        </div>
+
+        {isError && (
+          <span className={style.error}>
+            Invalid credentials. Please try again...
+          </span>
+        )}
+
+        <button type="submit" className={style.button}>
+          Log in
+        </button>
+      </form>
+    </div>
+  );
+}
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,36 +1,10 @@
-"use client";
-
 import style from "./page.module.css";
 import Link from "next/link";
-import { signIn } from "next-auth/react";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import LoginForm from "../components/LoginForm";
+
+export const dynamic = 'force-static';
 
 export default function Login() {
-  const [uploaderInfo, setUploaderInfo] = useState({ email: "", password: "" });
-  const [isError, setIsError] = useState(false);
-  const router = useRouter();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const res = await signIn("credentials", {
-      email: uploaderInfo.email,
-      password: uploaderInfo.password,
-      redirect: false,
-    });
-
-    if (res.status === 200) {
-      console.log(res);
-      router.push("/");
-      router.refresh();
-      setIsError(false);
-    } else {
-      console.log(res.error);
-      setIsError(true);
-    }
-  };
-
   return (
     <main>
       <div className={style.container}>
@@ -45,46 +19,7 @@ export default function Login() {
             </span>{" "}
           </p>
         </div>
-        <div className={style.form_wrapper}>
-          <form onSubmit={handleSubmit} className={style.form}>
-            <div>
-              <label htmlFor="emailOrUsername">Email address or Username</label>
-              <input
-                className={style.input}
-                type="text"
-                name="emailOrUsername"
-                id="emailOrUsername"
-                value={uploaderInfo.email}
-                onChange={({ target }) =>
-                  setUploaderInfo({ ...uploaderInfo, email: target.value })
-                }
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input
-                className={style.input}
-                type="password"
-                name="password"
-                id="password"
-                value={uploaderInfo.password}
-                onChange={({ target }) =>
-                  setUploaderInfo({ ...uploaderInfo, password: target.value })
-                }
-              />
-            </div>
-
-            {isError && (
-              <span className={style.error}>
-                Invalid credentials. Please try again...
-              </span>
-            )}
-
-            <button type="submit" className={style.button}>
-              Log in
-            </button>
-          </form>
-        </div>
+        <LoginForm />
       </div>
     </main>
   );
